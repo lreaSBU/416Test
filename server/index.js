6618283adc66fd66264b9abc35cedd0452d43a17
Dr_Leez <Snargo@users.noreply.github.com>
2023-04-02 00:48:28 -0400

    cors fix 3

--- server/index.js
+++ server/index.js
@@ -11,7 +11,17 @@ const app = express()
 
 // SETUP THE MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+//app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.header('Origin'));
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 app.use(express.json())
 app.use(cookieParser())
 
