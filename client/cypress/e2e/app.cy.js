d7d5ddc6cd363d84c0eaeda1e65da43e4d5f5707
Dr_Leez <Snargo@users.noreply.github.com>
2023-04-02 22:26:36 -0400

    Splash screen rename 3

--- client/cypress/e2e/app.cy.js
+++ client/cypress/e2e/app.cy.js
@@ -5,6 +5,7 @@ describe('App Component', () => {
         cy.visit('https://finaltest1-front.onrender.com')
     })
 
+    
     it('sanity check that the welcome screen text is as it should be', () => {
         cy.get('#splashD').should('have.text', 'Welcome to Map Central: an online service for creating, editing, and sharing all your favorite maps')
     })
