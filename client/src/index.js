f4bbc4bcd2df73e161fcf7ee1d930d47bba93832
Dr_Leez <Snargo@users.noreply.github.com>
2023-04-08 14:09:50 -0400

    Build 2 Adding Back Files

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "@fontsource/work-sans";
+//import "@fontsource/work-sans";
 import reportWebVitals from './reportWebVitals';
 /*
   Entry-point for our application.
