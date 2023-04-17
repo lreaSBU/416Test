1d7a55cac2f2dc506bbddef525b369027ac5a197
jadsbu <jad.saddiqui@stonybrook.edu>
2023-04-17 10:20:37 -0400

    cypress test fix

--- client/cypress/e2e/app.cy.js
+++ client/cypress/e2e/app.cy.js
@@ -7,14 +7,14 @@ describe('App Component', () => {
 
     
     it('sanity check that the welcome screen text is as it should be', () => {
-        cy.get('#splashD').should('have.text', 'Welcome to Map Central: an online service for creating, editing, and sharing all your favorite maps')
+        cy.get('#splashD').should('have.text', 'Welcome to Map Central: This is an online service for creating, editing, and sharing all your favorite maps')
     })
 
     it('sanity check that the splashscreen credits are as they should be', () => {
-        cy.get('#splashC').should('have.text', 'Site by Spring 2023 CSE 416 Green Team')
+        cy.get('#splashC').should('have.text', 'Site created by Spring 2023 CSE 416 Green Team')
     })
 
     it('sanity check that the login button text is as it should be', () => {
-        cy.get('#splashB').should('have.text', 'Get Started')
+        cy.get('#splashL').should('have.text', 'Get Started')
     })
 })
